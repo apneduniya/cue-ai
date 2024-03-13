b5a056b91343fd4e8482736501d9210866a82f51
Pratik Guha Roy <159567314+PratikPorc@users.noreply.github.com>
2024-03-13 16:29:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,59 @@ import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 import json
+from gcsa.google_calendar import GoogleCalendar
+from gcsa.event import Event
+from datetime import datetime
+import os 
+import sys 
 
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+script_directory = os.path.dirname(os.path.abspath(sys.argv[0])) 
+
 
 prompt = """You are an entity recognizer where you have to recognize dates and the corresponding event as entity from the given txt file which is of a whatsapp chat export. The txt contains the date and time of when the chat messages were sent and the messages may also contain some date along with the event that is going to happen on that particular date. You are going to identify when an event is going to take place and which is that event from the messages.
 
 ----------------------
-Note:
-1. Output should also write the year along wiht the date
-2. Write the event briefly
 
-for example 1 the txt file will contain chats like this
-10/03/2024, 16:56 - Pratik: <Media omitted>
+RULES TO BE FOLLOWED:
+1. Output should also write the year along with the date
+2. Write the event briefly
+3. Your response should be in a VALID JSON in the said format.
+4. Don't hallucinate.
+
+----------------------
+
+For example 1 (the txt file will contain chats like this):
+
+PROMPT: 10/03/2024, 16:56 - Pratik: <Media omitted>
 10/03/2024, 17:03 - Pramit gdsc tiu: I am busy on 16th March because I will be having a party at my home.
 10/03/2024, 17:03 - Pramit gdsc tiu: What about you?
 10/03/2024, 17:03 - Pratik: we have to submit the idea for xshot hackathon by 15th March, 2024 and the contest is on 16th March
 10/03/2024, 17:04 - Pramit gdsc tiu: Gotcha
 
-this is a chat between Pratik(X) and Pramit(Y), talking about events idea for xshot hackathon(A), having a party(B), xshot hackathon(C)
-Generate the output after ignoring the dates and times written as "10/03/2024, 17:03" on the left hand side of the dash("-") as
 
-like
-15th March, 2024- Last date to submit idea for xshot hackathon.\n
-16th March, 2024- Pramit is busy and he will be having a party, The xshot hackathon is on that date.\n
+RESPONSE: 
 
+[
+{{
+"event_title": "Last date to submit idea for Xshot hackathon",
+"event_date": "15",
+"event_month": "3",
+"event_year": "2024",
+}},
+{{
+"event_title": "Xshot hackathon",
+"event_date": "16",
+"event_month": "3",
+"event_year": "2024",
+}}
+]
 -------------------
 
-for example 2 the txt file will contain chats like this
-10/03/2024, 23:46 - Srinjani goldfish: <Media omitted>
+For example 2 (the txt file will contain chats like this):
+PROMPT: 10/03/2024, 23:46 - Srinjani goldfish: <Media omitted>
 11/03/2024, 00:00 - Srinjani goldfish: Problem Solving Techniques Assignment
 JC - TIU JC-TIU
 
@@ -49,12 +72,42 @@ Please note:-
 
 
 this is a chat between Pratik(X) and Srinjani(Y), talking about events maths assignment(A), Chemistry assignment(B), Problem Solving Techniques Assignment(C)
-Generate the output after ignoring the dates and times written as "11/03/2024, 00:00" on the left hand side of the dash("-") as
 
-like
-14th March, 2024- Last date to submit maths assignment.\n
-15th March, 2024- Deadline for Chemistry assignment submission.\n
-23rd March, 2024- Deadline for Problem Solving Techniques Assignment.\n
+RESPONSE:
+
+[
+{{
+"event_title": "Last date to submit maths assignment",
+"event_date": "14",
+"event_month": "3",
+"event_year": "2024",
+}},
+{{
+"event_title": "Deadline for chemistry assignment submission",
+"event_date": "15",
+"event_month": "3",
+"event_year": "2024",
+}},
+{{
+"event_title": "Deadline for Problem Solving Techniques Assignment",
+"event_date": "24",
+"event_month": "3",
+"event_year": "2024",
+}}
+]
+
+--------------------------
+
+Give me response in JSON format like:
+[
+{{
+"event_title": "",
+"event_date": "",
+"event_month": "",
+"event_year": "",
+}}
+]
+
 """
 
 
@@ -62,12 +115,19 @@ def generate_gemini_content(text, prompt):
     model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content(prompt + text)
     generated_text = response.text
-    # Extracting dates and events from the generated text
-    events = []
-    for line in generated_text.split('\n'):
-            date, event = line.split("-", 1)
-            events.append({"date": date.strip(), "event": event.strip()})
-    return json.dumps(events)
+
+    return generated_text
+
+def save_event_in_google_calendar(event_name: str, event_date: int, event_month: int, event_year: int):
+    calendar = GoogleCalendar(credentials_path=f"{script_directory}/.credentials/credentials.json")
+
+    event = Event(
+        event_name,
+        start=datetime(event_year, event_month, event_date, 0, 0),
+        minutes_before_popup_reminder=15
+    )
+
+    calendar.add_event(event)
 
 
 st.title("Date Recognizer")
@@ -81,8 +141,36 @@ if uploaded_file is not None:
         st.success("Document uploaded successfully!")
 
         if st.button("Recognize Dates"):
+            st.write("Recognizing the events....")
             events_json = generate_gemini_content(text, prompt)
+            # st.json(json.loads(events_json))
+
+            # Saving event in google calender
+            st.write('Saving events in google calendar....')
+            for event in json.loads(events_json):
+                save_event_in_google_calendar(event_name=event["event_title"], event_date=int(event["event_date"]), event_month=int(event["event_month"]), event_year=int(event["event_year"]))
+
             st.markdown("## Recognized Dates: ")
             st.json(json.loads(events_json))
+
+
     except UnicodeDecodeError:
         st.error("Uploaded file is not a valid text document (.txt). Please try again.")
+
+footer="""<style>
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color:transparent;
+color: #FFFFFF;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>© 2024 ICue-AI. All Rights Reserved</p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
