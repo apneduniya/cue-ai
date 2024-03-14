7d7a443abe31f4d5a5610c6d5247436ae7577acd
Pratik Guha Roy <159567314+PratikPorc@users.noreply.github.com>
2024-03-14 23:45:14 +0530

    Create 2_💻_Developers.py

---
+++ pages/2_💻_Developers.py
@@ -0,0 +1,28 @@
+import streamlit as st
+st.set_page_config(
+    page_title="ICue Developers",
+    page_icon="💻",
+)
+st.markdown("""
+# Hello, I'm Pratik! 👋
+
+I am currently pursuing B.tech in Computer Science Engineering with a specialization in Artificial Intelligence at Techno India University. I am passionate about technology and its applications, with a keen interest in various domains including Machine Learning, UI/UX, and Web Development.
+
+## About Me
+
+- 🎓 Pursuing B.tech in CSE AI at Techno India University
+- 💼 Member of Google Developer Student Clubs (GDSC)
+- 🖥️ Learning Animation and 3D modelling
+- 🎙️ Former TEDCircles Speaker
+- 🌍 MUN (Model United Nations) community member
+
+            
+# Hello, I'm Adarsh! 👋
+            
+I am currently a human trying to act like a robot.
+
+## About Me
+
+- You don't have any other work?? You will get nothing by reading this....
+- You are still reading XD
+""")
