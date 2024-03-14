4a9b8c11c943f3e7e61f0d6e8a1fa3b0268d28bf
Pratik Guha Roy <159567314+PratikPorc@users.noreply.github.com>
2024-03-14 23:46:45 +0530

    Create 3_💬_ContactUs.py

---
+++ pages/3_💬_ContactUs.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.set_page_config(
+    page_title="ICue Contacts",
+    page_icon="💬",
+)
+st.markdown("""
+## Contact Us
+
+
+- **Pratik**
+  - GitHub: [PratikPorc](https://github.com/PratikPorc)
+  - LinkedIn: [Pratik Guha Roy](https://www.linkedin.com/in/pratik-guha-roy-948008279/)
+
+- **Adarsh**
+  - GitHub: [apneduniya](https://github.com/apneduniya)
+  - LinkedIn: [thatsmeadarsh](https://www.linkedin.com/in/thatsmeadarsh)
+"""
+)
